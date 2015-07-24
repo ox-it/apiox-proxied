67e7ab7684d129518f3fffc0f12fabfacab96e98
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-07-24 13:35:41 +0100

    Initial commit

---
+++ apiox/proxied/__init__.py
@@ -0,0 +1,47 @@
+import os
+
+
+from ..core.handlers import ReverseProxyHandler
+
+prefix = 'person'
+url_prefix = '/{}/'.format(prefix)
+
+apis = {
+    'library': {
+        'title': 'Library API',
+        'href': 'http://api.m.ox.ac.uk/library/',
+    },
+    'contacts': {
+        'title': 'Contact Search API',
+        'href': 'http://api.m.ox.ac.uk/contact/',
+        'path': 'contact',
+    },
+    'river-status': {
+        'title': 'River Status API',
+        'href': 'http://api.m.ox.ac.uk/rivers/',
+        'path': 'rivers',
+    },
+    'transport': {
+        'title': 'Transport Information API',
+        'href': 'http://api.m.ox.ac.uk/transport/',
+    },
+    'places': {
+        'title': 'Places API',
+        'href': 'http://api.m.ox.ac.uk/places/',
+    },
+}
+
+def hook_in(app):
+    for name in apis:
+        definition = apis[name].copy()
+        del definition['href']
+        path = definition.pop('path', name)
+        app['definitions'][name] = definition
+
+        app.router.add_route('*', '/{path}/{{path:.*}}'.format(path=path),
+                             ReverseProxyHandler(apis[name]['href']),
+                             name='{name}:reverse-proxy'.format(name=name))
+        app.router.add_route('*', '/{path}/'.format(path=path),
+                             lambda request: None,
+                             name='{name}:index'.format(name=name))
+
